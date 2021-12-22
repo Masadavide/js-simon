786c5f1a360a2ed2b242cb64c767a5c5c5f013ca
Masadavide <davide.masadm@gmail.com>
2021-12-22 17:52:50 +0100

    base ok, fare css e limitare doppioni ricordati

--- myscript.js
+++ myscript.js
@@ -6,3 +6,51 @@ Consigli del giorno:
 * Pensate prima in italiano.
 * Dividete in piccoli problemi la consegna.
 * Individuate gli elementi di cui avete bisogno per realizzare il programma. */
+
+
+
+function generaNumeriCasuali (numero){
+    while(numeriCasualiArray.length < numero){
+        let num = Math.floor((Math.random()) * 100 + 1);
+        if(!numeriCasualiArray.includes(num)){
+            numeriCasualiArray.push(num);
+        }else{
+            console.log("doppione " + num);
+        }
+    }
+    numeriHtml.innerHTML = numeriCasualiArray;
+}
+
+function sparireNumeri(){
+    document.getElementById('numeri').classList.add('displaynone');
+}
+
+function inserisciPrompt(){
+    for(i = 0; i < 5; i++) {
+        let numeriInseriti = parseInt(prompt('inserisci numero'))
+        console.log(numeriInseriti)
+        numeriInseritiArray.push(numeriInseriti)
+        if(numeriCasualiArray.includes(numeriInseriti)){
+            numeriCorrettiArray.push(numeriInseriti)
+            console.log(numeriCorrettiArray)
+            numeriHtml.innerHTML = numeriCorrettiArray
+            document.getElementById('numeri').classList.remove('displaynone');
+        }
+    }
+    console.log(numeriInseritiArray)
+}
+
+
+
+setTimeout(sparireNumeri, 3000);
+
+setTimeout(inserisciPrompt, 3010);
+
+let numeriCasualiArray = [];
+let numeriHtml = document.getElementById("numeri");
+let numeriInseritiArray = [];
+let numeriCorrettiArray = [];
+
+
+generaNumeriCasuali (5);
+console.log(numeriCasualiArray);
